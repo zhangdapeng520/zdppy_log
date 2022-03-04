dd9c2a67c9a1b987daf94920fbd1627a3599c81e
zhangdapeng520 <pygosuperman@outlook.com>
2022-03-04 16:33:42 +0800

    版本0.1.3

--- zdppy_log/zdppy_log.py
+++ zdppy_log/zdppy_log.py
@@ -20,11 +20,15 @@ class Log:
 
     def __init__(self, log_file_path: str = "logs/zdppy/zdppy_log.log",
                  level: str = "INFO",
+                 format:str = "<green>{time:YYYY-MM-DD HH:mm:ss}</green> | <level>{level}</level> | <cyan>{extra[module_name]}</cyan>:<cyan>{extra[func_name]}</cyan>:<cyan>{extra[line_no]}</cyan> | <level>{message}</level>",
                  rotation: str = "100 MB",
                  serialize: bool = False,
                  retention: int = 10,
                  debug: bool = True
                  ):
+        """
+        创建日志对象
+        """
         # 初始化日志
         logger.remove()
 
@@ -44,13 +48,20 @@ class Log:
         self.retention = retention
         config["retention"] = retention
 
+        # 日志格式
+        self.format = format
+
         # 是否为debug模式
         self.__debug = debug
 
-        # 创建日志
+        # 创建文件日志
+        # 颜色说明：green 绿色 level 等级颜色 cyan 天蓝色
+        config["format"] = format
         logger.add(log_file_path, **config)
+
+        # 创建控制台日志
         if self.__debug:
-            logger.add(sys.stderr, level="DEBUG")
+            logger.add(sys.stderr, level="DEBUG", format=format)
         self.log = logger
 
     def catch(self):
@@ -60,17 +71,71 @@ class Log:
         """
         return self.log.catch()
 
+    def __get_log_where(self):
+        """
+        获取日志的路径信息
+        """
+        result = {}  # 存储信息
+
+        # 获取被调用函数所在模块文件名
+        module_name = sys._getframe(1).f_code.co_filename
+        result["module_name"] = module_name
+
+        # 获取被调用函数名称
+        func_name = sys._getframe(1).f_code.co_name
+        result["func_name"] = func_name
+
+        # 获取被调用函数在被调用时所处代码行数
+        line_no = sys._getframe(1).f_lineno
+        result["line_no"] = line_no
+
+        # 返回结果
+        return result
+
     def debug(self, *args, **kwargs):
-        return self.log.debug(*args, **kwargs)
+        """
+        记录DEBUG级别的日志
+        """
+        info = self.__get_log_where()
+        extra_logger = self.log.bind(**info)
+        return extra_logger.debug(*args, **kwargs)
 
     def info(self, *args, **kwargs):
-        return self.log.info(*args, **kwargs)
+        """
+        记录INFO级别的日志
+        """
+        info = self.__get_log_where()
+        extra_logger = self.log.bind(**info)
+        return extra_logger.info(*args, **kwargs)
+
+    def success(self, *args, **kwargs):
+        """
+        记录SUCCESS级别的日志
+        """
+        info = self.__get_log_where()
+        extra_logger = self.log.bind(**info)
+        return extra_logger.success(*args, **kwargs)
 
     def warning(self, *args, **kwargs):
-        return self.log.warning(*args, **kwargs)
+        """
+        记录WARNING级别的日志
+        """
+        info = self.__get_log_where()
+        extra_logger = self.log.bind(**info)
+        return extra_logger.warning(*args, **kwargs)
 
     def error(self, *args, **kwargs):
-        return self.log.error(*args, **kwargs)
+        """
+        记录ERROR级别的日志
+        """
+        info = self.__get_log_where()
+        extra_logger = self.log.bind(**info)
+        return extra_logger.error(*args, **kwargs)
 
     def critical(self, *args, **kwargs):
-        return self.log.critical(*args, **kwargs)
+        """
+        记录CRITICAL级别的日志
+        """
+        info = self.__get_log_where()
+        extra_logger = self.log.bind(**info)
+        return extra_logger.critical(*args, **kwargs)
